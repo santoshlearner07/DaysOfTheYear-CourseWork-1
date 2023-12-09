99c3b181437a4a2ab9e3543465393bd70aa4a363
Santosh <nandiyawarsantosh.0719@gmail.com>
2023-12-09 06:19:09 +0530

    Fixed:- Date should not increase cross final month date

--- DaysOfTheYear.java
+++ DaysOfTheYear.java
@@ -4,6 +4,8 @@ public class DaysOfTheYear {
 
   private static Scanner sc = new Scanner(System.in);
   private static int currentDay = 1;
+  private static int currentSuffixDay;
+  private static int currentSuffixMonth;
   private static int currentMonth = 1;
   private static int player = 0;
   static final int MAX_DAY = 31;
@@ -11,10 +13,12 @@ public class DaysOfTheYear {
   static final int MIN_DAY = 1;
   static final int MAX_MONTH = 12;
   static final int MIN_MONTH = 1;
+  static final int ELEVENTH_DAY = 11;
+  static final int THIRTEENTH_DAY = 13;
+  static final int SUFFIX_DIVISION = 10;
   private static int monthValidate;
   private static int day;
-  private static String userEntryDay;
-  private static String userEntryMonth;
+  private static String userEntry;
   static final int ZERO = 0;
   static final int FEB_MONTH_DAY = 28;
   static final int FEB_LEAP_MONTH_DAY = 29;
@@ -45,6 +49,7 @@ public class DaysOfTheYear {
       "December",
   };
 
+  // player increment where even = player 2 and odd = player 1
   static void player() {
     if (player % 2 == 0) {
       player = 2;
@@ -53,185 +58,31 @@ public class DaysOfTheYear {
     }
   }
 
-  static void checkingDay() {
-    userEntryDay = sc.next();
-
-    while (!checkInput(userEntryDay)) {
+  static void handlingThirdLevel() {
+    // // this loop will run if user puts words
+    // System.out.println(day + "day64");
+    while (!checkInput(userEntry)) {
       System.out.println("Input invalid, please try again!");
-      userEntryDay = sc.next();
+      userEntry = sc.next();
     }
-    day = Integer.parseInt(userEntryDay);
-    if ((currentMonth == SECOND_MONTH) && (day > FEB_MONTH_DAY)) {
-      System.out.println("Input invalid, please try again!");
-      checkingDay();
-    } else if (MIN_DAY <= day && day <= MAX_DAY) {
-      if (currentDay <= day) {
-        currentDay = day;
-      } else {
-        System.out.println("Input invalid, please try again!");
-        checkingDay();
-      }
-    } else {
-      System.out.println("Input invalid, please try again!");
-      checkingDay();
-    }
-  }
-
-  static void checkingMonth() {
-    userEntryMonth = sc.next();
-    while (!checkInputMonth(userEntryMonth)) {
-      System.out.println("Input invalid, please try again!");
-      userEntryMonth = sc.next();
-    }
-    monthValidate = Integer.parseInt(userEntryMonth);
-    if (((currentDay == FEB_LEAP_MONTH_DAY
-        || currentDay == SECOND_LAST_DAY
-        || currentDay == MAX_DAY)
-        &&
-        (monthValidate == SECOND_MONTH))
-        || ((currentDay == MAX_DAY) && ((monthValidate == FOURTH_MONTH)
-            || (monthValidate == SIXTH_MONTH)
-            || (monthValidate == NINETH_MONTH)
-            || (monthValidate == ELEVEN_MONTH)))) {
-      System.out.println("Input invalid, please try again!");
-      checkingMonth();
-    } else if ((MIN_DAY <= monthValidate) && (monthValidate <= MAX_MONTH)) {
-      if (currentMonth < monthValidate) {
-        currentMonth = monthValidate;
-      } else {
-        System.out.println("Input invalid, please try again!");
-        checkingMonth();
-      }
-    } else {
-      System.out.println("Input invalid, please try again!");
-      checkingMonth();
-    }
-  }
-
-  static boolean checkInput(String userEntryDay) {
-    boolean hasString = false;
-    int index = 0;
-    while (index < userEntryDay.length()) {
-      if ((userEntryDay.charAt(index) >= '0')
-          && (userEntryDay.charAt(index) <= '9')) {
-        hasString = true;
-      }
-      index++;
-    }
-    return hasString;
-  }
-
-  static boolean checkInputMonth(String userEntryMonth) {
-    boolean hasString = false;
-    int index = 0;
-    while (index < userEntryMonth.length()) {
-      if ((userEntryMonth.charAt(index) >= '0')
-          && (userEntryMonth.charAt(index) <= '9')) {
-        hasString = true;
-      }
-      index++;
-    }
-    return hasString;
-  }
-
-  static void caseMonth() {
-    System.out.print("Which month do you want to pick: ");
-    userEntryMonth = sc.next();
-    while (!checkInputMonth(userEntryMonth)) {
-      System.out.println("Input invalid, please try again!");
-      userEntryMonth = sc.next();
-    }
-    monthValidate = Integer.parseInt(userEntryMonth);
-    if (((currentDay == FEB_LEAP_MONTH_DAY
+    // // converts string number to integer
+    day = Integer.parseInt(userEntry);
+    if ((((currentMonth == SECOND_MONTH) || (currentSuffixMonth == SECOND_MONTH))
+        && (day > FEB_MONTH_DAY))
         ||
-        currentDay == SECOND_LAST_DAY
-        ||
-        currentDay == MAX_DAY)
-        &&
-        (monthValidate == SECOND_MONTH))
-        ||
-        ((currentDay == MAX_DAY)
-            &&
-            ((monthValidate == FOURTH_MONTH)
-                ||
-                (monthValidate == SIXTH_MONTH)
-                ||
-                (monthValidate == NINETH_MONTH)
-                ||
-                (monthValidate == ELEVEN_MONTH)))) {
-      System.out.println("Input invalid, please try again!");
-      checkingMonth();
-    } else if ((MIN_MONTH <= monthValidate) && (monthValidate <= MAX_MONTH)) {
-      if (currentMonth < monthValidate) {
-        currentMonth = monthValidate;
-      } else {
-        System.out.println("Input invalid, please try again!");
-        checkingMonth();
-      }
-    } else {
-      System.out.println("Input invalid, please try again!");
-      checkingMonth();
-    }
-  }
-
-  static void tempDay() {
-    userEntryDay = sc.next();
-    while (!checkInput(userEntryDay)) {
-      System.out.println("Input invalid, please try again!");
-      userEntryDay = sc.next();
-    }
-    day = Integer.parseInt(userEntryDay);
-
-    if (day == MAX_DAY) {
-      System.out.println("Input invalid, please try again!");
-      tempDay();
-    } else if (((currentMonth == SECOND_MONTH) && (day > FEB_MONTH_DAY))
-        ||
-        (((currentMonth == FOURTH_MONTH)
-            ||
-            (currentMonth == SIXTH_MONTH)
-            ||
-            (currentMonth == NINETH_MONTH)
-            ||
-            (currentMonth == ELEVEN_MONTH))
-            &&
-            (day > SECOND_LAST_DAY))) {
+        (((currentMonth == FOURTH_MONTH) || (currentMonth == SIXTH_MONTH)
+            || (currentMonth == NINETH_MONTH) || (currentMonth == ELEVEN_MONTH))
+            && (day > SECOND_LAST_DAY))) {
       System.out.println("Input invalid, please try again!");
 
-      caseDay();
-    } else {
-      currentDay = SECOND_LAST_DAY;
-    }
-  }
-
-  static void caseDay() {
-    System.out.print("Which day do you want to pick: ");
-    userEntryDay = sc.next();
-
-    while (!checkInput(userEntryDay)) {
-      System.out.println("Input invalid, please try again!");
-      userEntryDay = sc.next();
-    }
-    day = Integer.parseInt(userEntryDay);
-    if (((currentMonth == SECOND_MONTH) && (day > FEB_MONTH_DAY))
-        ||
-        (((currentMonth == FOURTH_MONTH)
-            ||
-            (currentMonth == SIXTH_MONTH)
-            ||
-            (currentMonth == NINETH_MONTH)
-            ||
-            (currentMonth == ELEVEN_MONTH))
-            &&
-            (day > SECOND_LAST_DAY))) {
-      System.out.println("Input invalid, please try again!");
-
+      // // till the user puts correct day it will call this method everytime
       tempDay();
     } else if (MIN_DAY <= day && day <= MAX_DAY) {
       if (currentDay < day) {
         currentDay = day;
       } else {
         System.out.println("Input invalid, please try again!");
+        // // till the user puts correct day it will call this method everytime
         checkingDay();
       }
     } else {
@@ -240,6 +91,100 @@ public class DaysOfTheYear {
     }
   }
 
+  // method to check if day is not selected correctly
+  static void checkingDay() {
+    userEntry = sc.next();
+    handlingThirdLevel();
+  }
+
+  // method to check if month is not selected correctly
+  static void checkingMonth() {
+    userEntry = sc.next();
+    // this loop will run if user puts words
+    while (!checkInput(userEntry)) {
+      System.out.println("Input invalid, please try again!");
+      userEntry = sc.next();
+    }
+    // converts string number to integer
+    monthValidate = Integer.parseInt(userEntry);
+
+    if (((currentDay == FEB_LEAP_MONTH_DAY || currentDay == SECOND_LAST_DAY
+        || currentDay == MAX_DAY)
+        &&
+        (monthValidate == SECOND_MONTH))
+        || ((currentDay == MAX_DAY) && ((monthValidate == FOURTH_MONTH)
+            || (monthValidate == SIXTH_MONTH) || (monthValidate == NINETH_MONTH)
+            || (monthValidate == ELEVEN_MONTH)))) {
+      System.out.println("Input invalid, please try again!");
+      // till the user puts correct day it will call this method everytime
+      checkingMonth();
+    } else if ((MIN_DAY <= monthValidate) && (monthValidate <= MAX_MONTH)) {
+      if (currentMonth < monthValidate) {
+        currentMonth = monthValidate;
+      } else {
+        System.out.println("Input invalid, please try again!");
+        // till the user puts correct day it will call this method everytime
+        checkingMonth();
+      }
+    } else {
+      System.out.println("Input invalid, please try again!");
+      // till the user puts correct day it will call this method everytime
+      checkingMonth();
+    }
+  }
+
+  // this method gets called if user puts alphabets for the day/month
+  static boolean checkInput(String userEntry) {
+    boolean hasString = false;
+    int index = 0;
+    while (index < userEntry.length()) {
+      if ((userEntry.charAt(index) >= '0')
+          && (userEntry.charAt(index) <= '9')) {
+        hasString = true;
+      }
+      index++;
+    }
+    return hasString;
+  }
+
+  // switch month case will get called
+  static void caseMonth() {
+    System.out.print("Which month do you want to pick: ");
+    checkingMonth();
+  }
+
+  // it checks if user entered 29 for feb and 31 for 30 days of month
+  static void tempDay() {
+    userEntry = sc.next();
+    while (!checkInput(userEntry)) {
+      System.out.println("Input invalid, please try again!");
+      userEntry = sc.next();
+    }
+    day = Integer.parseInt(userEntry);
+
+    if (((day == MAX_DAY) || (day == SECOND_LAST_DAY) && (monthValidate == SECOND_MONTH))) {
+      System.out.println("Input invalid, please try again!");
+      tempDay();
+    } else if (((currentMonth == SECOND_MONTH) && (day > FEB_MONTH_DAY))
+        || (((currentMonth == FOURTH_MONTH) || (currentMonth == SIXTH_MONTH)
+            || (currentMonth == NINETH_MONTH) || (currentMonth == ELEVEN_MONTH))
+            &&
+            (day > SECOND_LAST_DAY))) {
+      System.out.println("Input invalid, please try again!");
+
+      caseDay();
+    } else {
+      currentDay = day;
+    }
+  }
+
+  // switch day case will get called
+  static void caseDay() {
+    System.out.print("Which day do you want to pick: ");
+    checkingDay();
+  }
+
+  // switch case for month and day
   static void selectedMonthDay() {
     String selectDayOrMonth = sc.next();
     switch (selectDayOrMonth.toLowerCase()) {
@@ -255,40 +200,92 @@ public class DaysOfTheYear {
     }
   }
 
+  static String suffixDay(String[] args) {
+    String only = "th";
+    if (args.length == 2) {
+      if (currentDay >= ELEVENTH_DAY && currentDay <= THIRTEENTH_DAY) {
+        return "th";
+      }
+      switch (day % SUFFIX_DIVISION) {
+        case FIRST_MONTH:
+          return "st";
+        case SECOND_MONTH:
+          return "nd";
+        case THIRD_MONTH:
+          return "rd";
+        default:
+          return "th";
+      }
+    }
+    return only;
+  }
+
+  // the main code starts here
   public static void main(String[] args) {
     while (true) {
       player = player + 1;
       if (currentDay == MAX_DAY && currentMonth == MAX_MONTH) {
         if (currentMonth == MAX_MONTH) {
-          System.out.println(
-              "The current date is: "
-                  +
-                  currentDay
-                  +
-                  " of "
-                  +
-                  ALL_MONTH_OF_YEAR[currentMonth - MIN_MONTH]);
+          if (args.length == 2) {
+            System.out.println(
+                "The current date is: " + currentDay + suffixDay(args) + " of "
+                    + ALL_MONTH_OF_YEAR[currentMonth - MIN_MONTH]);
+          } else {
+            System.out.println(
+                "The current date is: " + currentDay + " of "
+                    + ALL_MONTH_OF_YEAR[currentMonth - MIN_MONTH]);
+          }
         }
+
         System.out.println(
             "Player " + (player - 1) + " is the winner of the game!");
         player();
         break;
       } else {
         player();
-        System.out.println(
-            "The current date is: "
-                +
-                currentDay
-                +
-                " of "
-                +
-                ALL_MONTH_OF_YEAR[currentMonth - MIN_MONTH]);
+        if (args.length == 2) {
+          currentSuffixDay = Integer.parseInt(args[0]);
+          currentSuffixMonth = Integer.parseInt(args[1]);
+          if (currentDay <= currentSuffixDay && currentMonth <= currentSuffixMonth) {
+            System.out.println(
+                "The current date is: " + currentSuffixDay + suffixDay(args) + " of "
+                    + ALL_MONTH_OF_YEAR[currentSuffixMonth - MIN_MONTH]);
+          } else {
+            // currentSuffixDay = 0;
+            // currentSuffixMonth = 0;
+            if (currentMonth == MIN_MONTH) {
+              System.out.println(
+                  "The current date is: " + currentDay + suffixDay(args) + " of "
+                      + ALL_MONTH_OF_YEAR[currentSuffixMonth - MIN_MONTH]);
+            } else {
+              currentSuffixMonth = 0;
+              currentSuffixMonth = monthValidate;
+              System.out.println(
+                  "The current date is: " + currentDay + suffixDay(args) + " of "
+                      + ALL_MONTH_OF_YEAR[currentSuffixMonth - MIN_MONTH]);
+            }
+          }
+        } else {
+          System.out.println(
+              "The current date is: " + currentDay + " of "
+                  + ALL_MONTH_OF_YEAR[currentMonth - MIN_MONTH]);
+        }
         System.out.println("It is Player " + player + "'s Turn!");
         System.out.print(
             "Do you want to increase the day or the month? (day or month): ");
+
+        // if (args.length == 2) {
+        //   if ((currentDay > FEB_LEAP_MONTH_DAY) && ((currentSuffixMonth == SECOND_MONTH))) {
+        //     System.out.print("Can't change the day, please try again!");
+        //     selectedMonthDay();
+        //   } else {
+        //     selectedMonthDay();
+        //   }
+        // } 
         selectedMonthDay();
       }
     }
+    // closing the scanner
     sc.close();
   }
 }
