9ad8b20f6e7fc338c3963780f4784db29bf17ce0
Santosh <nandiyawarsantosh.0719@gmail.com>
2023-12-09 06:11:40 +0530

    Input Invalid problem

--- DaysOfTheYear.java
+++ DaysOfTheYear.java
@@ -6,8 +6,15 @@ public class DaysOfTheYear {
   static int currentDay = 1;
   static int currentMonth = 1;
   static int player = 0;
+  static int maxDay = 31;
+  static int minDay = 1;
+  static int maxMonth = 12;
+  static int minMonth = 1;
+  static int monthValidate;
+  static int day;
   static String userEntryDay;
   static String userEntryMonth;
+  static String lastMonth = "December";
 
   static String[] allMonth = {
     "January",
@@ -24,8 +31,6 @@ public class DaysOfTheYear {
     "December",
   };
 
-  static String lastMonth = "December";
-
   static void player() {
     if (player % 2 == 0) {
       player = 2;
@@ -41,12 +46,16 @@ public class DaysOfTheYear {
       System.out.println("Input invalid, please try again!");
       userEntryDay = sc.next();
     }
-    int day = Integer.parseInt(userEntryDay);
-    if (0 < day && day < 32) {
+    day = Integer.parseInt(userEntryDay);
+    if ((currentMonth == 2) && (day > 28)) {
+      System.out.println("Input invalid, please try again!");
+      checkingDay();
+    } else if (minDay <= day && day <= maxDay) {
       if (currentDay <= day) {
         currentDay = day;
       } else {
         System.out.println("Input invalid, please try again!");
+        checkingDay();
       }
     } else {
       System.out.println("Input invalid, please try again!");
@@ -60,12 +69,30 @@ public class DaysOfTheYear {
       System.out.println("Input invalid, please try again!");
       userEntryMonth = sc.next();
     }
-    int monthValidate = Integer.parseInt(userEntryMonth);
-    if ((0 < monthValidate) && (monthValidate <= 12)) {
+    monthValidate = Integer.parseInt(userEntryMonth);
+    if (
+      (
+        (currentDay == 29 || currentDay == 30 || currentDay == maxDay) &&
+        (monthValidate == 2)
+      ) ||
+      (
+        (currentDay == maxDay) &&
+        (
+          (monthValidate == 4) ||
+          (monthValidate == 6) ||
+          (monthValidate == 9) ||
+          (monthValidate == 11)
+        )
+      )
+    ) {
+      System.out.println("Input invalid, please try again!");
+      checkingMonth();
+    } else if ((0 < monthValidate) && (monthValidate <= 12)) {
       if (currentMonth < monthValidate) {
         currentMonth = monthValidate;
       } else {
         System.out.println("Input invalid, please try again!");
+        checkingMonth();
       }
     } else {
       System.out.println("Input invalid, please try again!");
@@ -110,8 +137,25 @@ public class DaysOfTheYear {
       System.out.println("Input invalid, please try again!");
       userEntryMonth = sc.next();
     }
-    int monthValidate = Integer.parseInt(userEntryMonth);
-    if ((0 < monthValidate) && (monthValidate <= 12)) {
+    monthValidate = Integer.parseInt(userEntryMonth);
+    if (
+      (
+        (currentDay == 29 || currentDay == 30 || currentDay == maxDay) &&
+        (monthValidate == 2)
+      ) ||
+      (
+        (currentDay == maxDay) &&
+        (
+          (monthValidate == 4) ||
+          (monthValidate == 6) ||
+          (monthValidate == 9) ||
+          (monthValidate == 11)
+        )
+      )
+    ) {
+      System.out.println("Input invalid, please try again!");
+      checkingMonth();
+    } else if ((0 < monthValidate) && (monthValidate <= 12)) {
       if (currentMonth < monthValidate) {
         currentMonth = monthValidate;
       } else {
@@ -132,8 +176,22 @@ public class DaysOfTheYear {
       System.out.println("Input invalid, please try again!");
       userEntryDay = sc.next();
     }
-    int day = Integer.parseInt(userEntryDay);
-    if (0 < day && day < 32) {
+    day = Integer.parseInt(userEntryDay);
+    if (
+      ((currentMonth == 2) && (day > 28)) ||
+      (
+        (
+          (currentMonth == 4) ||
+          (currentMonth == 6) ||
+          (currentMonth == 9) ||
+          (currentMonth == 11)
+        ) &&
+        (day > 30)
+      )
+    ) {
+      System.out.println("Input invalid, please try again!");
+      selectedMonthDay();
+    } else if (minDay <= day && day <= maxDay) {
       if (currentDay < day) {
         currentDay = day;
       } else {
@@ -164,13 +222,13 @@ public class DaysOfTheYear {
   public static void main(String[] args) {
     while (true) {
       player = player + 1;
-      if (currentDay == 31 && currentMonth == 12) {
-        if (currentMonth == 12) {
+      if (currentDay == maxDay && currentMonth == maxMonth) {
+        if (currentMonth == maxMonth) {
           System.out.println(
             "The current date is: " +
             currentDay +
             " of " +
-            allMonth[currentMonth - 1]
+            allMonth[currentMonth - minMonth]
           );
         }
         System.out.println(
@@ -184,7 +242,7 @@ public class DaysOfTheYear {
           "The current date is: " +
           currentDay +
           " of " +
-          allMonth[currentMonth - 1]
+          allMonth[currentMonth - minMonth]
         );
         System.out.println("It is Player " + player + "'s Turn!");
         System.out.print(
